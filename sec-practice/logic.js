ee92462f9cdaad6121307d58fb566ed229fdbbb6
John Britos <johnbritosash@gmail.com>
2025-01-06 19:35:43 +0800

    bukas ulit

--- sec-practice/logic.js
+++ sec-practice/logic.js
@@ -1,1 +1,21 @@
-console.log("hello");
+function getRandomNumber(){
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    return randomNumber;
+}
+
+console.log(getRandomNumber());
+
+
+function playerGuess(number){
+    let computerNumber = getRandomNumber();    
+    playerNumber = document.getElementById(number);
+
+    if (playerNumber === computerNumber) {
+        alert(`You Guessed ${computerNumber} and you are right! are you a prophet!`);        
+    } else {
+        alert(`You lost bitch the number is ${computerNumber}`);
+    }
+    
+    
+}
+
